1a64920996bd948687e775159f7f9ba388676ab9
dreamerminsk <tutmirage@gmail.com>
2020-05-27 19:55:13 +0300

    refactoring

---
+++ pe.js
@@ -0,0 +1,223 @@
+const readBuffer = async(inputFile, fromByte, toByte) => {
+    const temporaryFileReader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+        temporaryFileReader.onerror = () => {
+            temporaryFileReader.abort();
+            reject(new DOMException("Problem parsing input file."));
+        };
+
+        temporaryFileReader.onload = () => {
+            resolve(temporaryFileReader.result);
+        };
+        var blob = inputFile.slice(fromByte, toByte);
+        temporaryFileReader.readAsArrayBuffer(blob);
+    });
+};
+
+
+const readUploadedFile = async(inputFile) => {
+    const temporaryFileReader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+        temporaryFileReader.onerror = () => {
+            temporaryFileReader.abort();
+            reject(new DOMException("Problem parsing input file."));
+        };
+
+        temporaryFileReader.onload = () => {
+            resolve(temporaryFileReader.result);
+        };
+
+        temporaryFileReader.readAsArrayBuffer(inputFile);
+    });
+};
+
+
+
+
+async function readDosHeader(buf) {
+    return {
+        e_lfanew: new Uint32Array(buf, 0)[15],
+    };
+}
+
+async function readSignature(buf, offset) {
+    let utf8decoder = new TextDecoder();
+    let buffer = new Uint8Array(buf, offset, 4);
+    return {
+        Signature: `${utf8decoder.decode(buffer)}\\${buffer[2]}\\${buffer[3]}`,
+    };
+}
+
+async function readFileHeader(buf, offset) {
+    return {
+        Machine: new Uint16Array(buf, offset)[0],
+        NumberOfSections: new Uint16Array(buf, offset)[1],
+        TimeDateStamp: new Uint32Array(buf, offset)[1],
+        PointerToSymbolTable: new Uint32Array(buf, offset)[2],
+        NumberOfSymbols: new Uint32Array(buf, offset)[3],
+        SizeOfOptionalHeader: new Uint16Array(buf, offset)[8],
+        Characteristics: new Uint16Array(buf, offset)[9],
+    };
+}
+
+async function readOptHeader(buf, offset, size) {
+    let b = new DataView(buf, offset);
+    let magic = b.getUint16(0, true);
+    if (magic == 267) {
+        return await readOptHeader32(b);
+    } else if (magic == 523) {
+        return await readOptHeader64(b);
+    } else {
+        return {
+            magic: magic
+        };
+    }
+}
+
+async function readOptHeader32(buf) {
+    return {
+        Magic: buf.getUint16(0, true).toString(16),
+        MajorLinkerVersion: buf.getUint8(2, true),
+        MinorLinkerVersion: buf.getUint8(3, true),
+        SizeOfCode: buf.getUint32(4, true),
+        SizeOfInitializedData: buf.getUint32(8, true),
+        SizeOfUninitializedData: buf.getUint32(12, true),
+        AddressOfEntryPoint: buf.getUint32(16, true),
+        BaseOfCode: buf.getUint32(20, true),
+        BaseOfData: buf.getUint32(24, true),
+        ImageBase: buf.getUint32(28, true),
+        SectionAlignment: buf.getUint32(32, true),
+        FileAlignment: buf.getUint32(36, true),
+        MajorOperatingSystemVersion: buf.getUint16(40, true),
+        MinorOperatingSystemVersion: buf.getUint16(42, true),
+        MajorImageVersion: buf.getUint16(44, true),
+        MinorImageVersion: buf.getUint16(46, true),
+        MajorSubsystemVersion: buf.getUint16(48, true),
+        MinorSubsystemVersion: buf.getUint16(50, true),
+        Win32VersionValue: buf.getUint32(52, true),
+        SizeOfImage: buf.getUint32(56, true),
+        SizeOfHeaders: buf.getUint32(60, true),
+        CheckSum: buf.getUint32(64, true),
+        Subsystem: buf.getUint16(68, true),
+        DllCharacteristics: '0x' + buf.getUint16(70, true).toString(16),
+        SizeOfStackReserve: buf.getUint32(72, true),
+        SizeOfStackCommit: buf.getUint32(76, true),
+        SizeOfHeapReserve: buf.getUint32(80, true),
+        SizeOfHeapCommit: buf.getUint32(84, true),
+        LoaderFlags: buf.getUint32(88, true),
+        NumberOfRvaAndSizes: buf.getUint32(92, true),
+    };
+}
+
+
+async function readOptHeader64(buf) {
+    return {
+        Magic: buf.getUint16(0, true).toString(16),
+        MajorLinkerVersion: buf.getUint8(2, true),
+        MinorLinkerVersion: buf.getUint8(3, true),
+        SizeOfCode: buf.getUint32(4, true),
+        SizeOfInitializedData: buf.getUint32(8, true),
+        SizeOfUninitializedData: buf.getUint32(12, true),
+        AddressOfEntryPoint: buf.getUint32(16, true),
+        BaseOfCode: buf.getUint32(20, true),
+        ImageBase: buf.getBigUint64(24, true),
+        SectionAlignment: buf.getUint32(32, true),
+        FileAlignment: buf.getUint32(36, true),
+        MajorOperatingSystemVersion: buf.getUint16(40, true),
+        MinorOperatingSystemVersion: buf.getUint16(42, true),
+        MajorImageVersion: buf.getUint16(44, true),
+        MinorImageVersion: buf.getUint16(46, true),
+        MajorSubsystemVersion: buf.getUint16(48, true),
+        MinorSubsystemVersion: buf.getUint16(50, true),
+        Win32VersionValue: buf.getUint32(52, true),
+        SizeOfImage: buf.getUint32(56, true),
+        SizeOfHeaders: buf.getUint32(60, true),
+        CheckSum: buf.getUint32(64, true),
+        Subsystem: buf.getUint16(68, true),
+        DllCharacteristics: '0x' + buf.getUint16(70, true).toString(16),
+        SizeOfStackReserve: buf.getBigUint64(72, true),
+        SizeOfStackCommit: buf.getBigUint64(80, true),
+        SizeOfHeapReserve: buf.getBigUint64(88, true),
+        SizeOfHeapCommit: buf.getBigUint64(96, true),
+        LoaderFlags: buf.getUint32(104, true),
+        NumberOfRvaAndSizes: buf.getUint32(108, true),
+    };
+}
+
+function getDirName(idx) {
+    switch (idx) {
+        case 0:
+            return "ENTRY_EXPORT";
+        case 1:
+            return "ENTRY_IMPORT";
+        case 2:
+            return "ENTRY_RESOURCE";
+        case 3:
+            return "ENTRY_EXCEPTION";
+        case 4:
+            return "ENTRY_SECURITY";
+        case 5:
+            return "ENTRY_BASERELOC";
+        case 6:
+            return "ENTRY_DEBUG";
+        case 7:
+            return "ENTRY_ARCHITECTURE";
+        case 8:
+            return "ENTRY_GLOBALPTR";
+        case 9:
+            return "ENTRY_TLS";
+        case 10:
+            return "ENTRY_LOAD_CONFIG";
+        case 11:
+            return "ENTRY_BOUND_IMPORT";
+        case 12:
+            return "ENTRY_IAT";
+        case 13:
+            return "ENTRY_DELAY_IMPORT";
+        case 14:
+            return "ENTRY_COM_DESCRIPTOR";
+        case 15:
+            return "ENTRY_RESERVED";
+        default:
+            return "ENTRY_UNKNOWN";
+    }
+}
+
+async function readDirectories(buf, count) {
+    let dirs = [];
+    console.log(buf);
+    for (let i = 0; i < count; i++) {
+        dirs.push({
+            VirtualAddress: buf.getUint32(8 * i, true),
+            Size: buf.getUint32(8 * i + 4, true),
+        });
+    }
+    return dirs;
+}
+
+async function readSections(buf, count) {
+    let dirs = [];
+    console.log(buf);
+    for (let i = 0; i < count; i++) {
+        dirs.push({
+            Name: await readString(buf, buf.byteOffset + 40 * i, 8),
+            VirtualSize: buf.getUint32(40 * i + 8, true),
+            VirtualAddress: buf.getUint32(40 * i + 12, true),
+            SizeOfRawData: buf.getUint32(40 * i + 16, true),
+            PointerToRawData: buf.getUint32(40 * i + 20, true),
+            PointerToRelocations: buf.getUint32(40 * i + 24, true),
+            PointerToLinenumbers: buf.getUint32(40 * i + 28, true),
+            NumberOfRelocations: buf.getUint16(40 * i + 32, true),
+            NumberOfLinenumbers: buf.getUint16(40 * i + 34, true),
+            Characteristics: buf.getUint32(40 * i + 36, true),
+        });
+    }
+    return dirs;
+}
+
+async function readString(buf, pos, length) {
+    let utf8decoder = new TextDecoder();
+    return utf8decoder.decode(new Uint8Array(buf.buffer, pos, length));
+}
---
+++ view.js
@@ -0,0 +1,152 @@
+async function processFile(f) {
+    const fileContentDiv = document.querySelector('div#file-content');
+    const fileContents = await readBuffer(f, 0, 1024);
+    try {
+        let dos = await readDosHeader(fileContents);
+        let sign = await readSignature(fileContents, dos.e_lfanew);
+        let fh = await readFileHeader(fileContents, dos.e_lfanew + 4);
+        let oh = await readOptHeader(fileContents, dos.e_lfanew + 24, fh.SizeOfOptionalHeader);
+        let dirOffset = dos.e_lfanew + 24 + (oh.Magic == "10b" ? 96 : 112);
+        let dirs = await readDirectories(
+            new DataView(fileContents, dirOffset),
+            oh.NumberOfRvaAndSizes);
+        let sectOffset = dirOffset + 8 * 16;
+        console.log(sectOffset);
+        let sects = await readSections(new DataView(fileContents, sectOffset), fh.NumberOfSections);
+
+        fileContentDiv.innerHTML =
+            `<div class='panel is-info is-small'>
+                <div class='panel-heading'>${f.Name}</div>
+                ${row("HEADERS", 0)}
+            </div>`;
+
+        fileContentDiv.innerHTML +=
+            `<div class='panel is-info is-small'>
+            <div class='panel-heading'>IMAGE_DOS_HEADER</div>
+            ${row("e_lfanew", dos.e_lfanew)}
+        </div>`;
+        fileContentDiv.innerHTML +=
+            `<div class='panel is-info is-small'>
+            <p class='panel-heading'>IMAGE_NT_HEADERS</p>
+            ${row("Signature", sign.Signature)}
+        </div>`;
+        fileContentDiv.innerHTML +=
+            `<div class='panel is-info is-small'>
+            <p class='panel-heading'>IMAGE_FILE_HEADER</p>
+            ${row("Machine", fh.Machine.toString(16))}
+            ${row("NumberOfSections", fh.NumberOfSections)}
+            ${row("TimeDateStamp", fh.TimeDateStamp.toString(16))}
+            ${row("PointerToSymbolTable", fh.PointerToSymbolTable)}
+            ${row("NumberOfSymbols", fh.NumberOfSymbols)}
+            ${row("SizeOfOptionalHeader", fh.SizeOfOptionalHeader)}
+            ${row("Characteristics", fh.Characteristics.toString(16))}
+        </div>`;
+        fileContentDiv.innerHTML +=
+            `<div class='panel is-info is-small'>
+            <p class='panel-heading'>IMAGE_OPTIONAL_HEADER</p>
+            ${Object.keys(oh).map((color, idx) => `
+                ${row(color, oh[color])}
+            `).join('')}
+        </div>`;
+
+        fileContentDiv.innerHTML +=
+            `<div class='panel is-info is-small'>
+            <p class='panel-heading'>IMAGE_DATA_DIRECTORIES</p>
+            ${dirs.map((color, idx) => `
+                ${entry_row(getDirName(idx), color.Size > 0)}
+                ${row("VirtualAddress", color.VirtualAddress, color.Size > 0)}
+                ${row("Size", color.Size, color.Size > 0)}
+            `).join('')}
+        </div>`;
+
+        fileContentDiv.innerHTML +=
+            `<div class='panel is-info is-small'>
+            <p class='panel-heading'>IMAGE_DATA_SECTIONS</p>
+            ${sects.map((sect, idx) => `
+                ${entry_row(sect.Name, true)}
+                ${Object.keys(sect).map((prop, idx) => `
+                ${row(prop, sect[prop])}
+            `).join('')}
+            `).join('')}
+        </div>`;
+    } catch (e) {
+        fileContentDiv.innerHTML += e.message
+    }
+};
+
+
+
+function entry_row(name, ispresent) {
+    return `<span class='panel-block'>
+        <div class='control'>
+            <p ${ispresent ?
+            "class=\'has-text-link has-text-weight-bold\'" :
+            "class=\'has-text-grey-light has-text-weight-bold\'"}>
+            ${name}
+            </p>
+        </div>
+    </span>            
+    `;
+}
+
+function row(name, value, ispresent = true) {
+    return `<span class='panel-block'>
+        <div class='control'>
+            <nav class='level is-mobile'>
+                <div class='level-left'>
+                    <div class='level-item'>
+                        <p class='has-text-left ${ispresent ? 'has-text-black' : 'has-text-grey-light'}'>${name}</p>
+                    </div>
+                </div>
+                <div class='level-right'>
+                    <div class='level-item'>
+                        <p class='has-text-right ${ispresent ? 'has-text-black' : 'has-text-grey-light'}'>${value}</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </span>            
+    `;
+}
+
+
+
+let dropArea = document.getElementById('drop-area');
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+};
+
+['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false);
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false);
+});
+
+function highlight(e) {
+    dropArea.classList.add('highlight');
+};
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight');
+};
+
+dropArea.addEventListener('drop', handleDrop, false);
+
+function handleDrop(e) {
+    let dt = e.dataTransfer;
+    let files = dt.files;
+    handleFiles(files);
+};
+
+function handleFiles(files) {
+    files = [...files];
+    files.forEach(processFile);
+};
